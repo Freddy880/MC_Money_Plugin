41b34161c5cbe42090cbf64f68789850158cd675
Freddy880 <81533091+Freddy880@users.noreply.github.com>
2021-06-06 14:43:45 +0200

    Changing License
    The license of the Class is added. These Class is from Github:
    From Github: https://github.com/ThexXTURBOXx/McUUIDFetcher
    (Latest acess: 06.06.2021)

--- src/main/java/de/freddy/tutorial/UUIDFetcher.java
+++ src/main/java/de/freddy/tutorial/UUIDFetcher.java
@@ -1,3 +1,31 @@
+/*
+LICENSE of these Class:
+MIT License
+
+Copyright (c) 2020 Nico Mexis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+© 2021 GitHub, Inc.
+
+From Github: https://github.com/ThexXTURBOXx/McUUIDFetcher
+(Latest acess: 06.06.2021)
+ */
 package de.freddy.tutorial;//package your.package.here;
 
 import java.io.BufferedReader;
@@ -28,32 +56,6 @@ public final class UUIDFetcher {
     private UUIDFetcher() {
         throw new UnsupportedOperationException();
     }
-
-    /**
-     * Returns the UUID of the searched player.
-     *
-     * @param player The player.
-     * @return The UUID of the given player.
-     */
-    //Uncomment this if you want the helper method for BungeeCord:
-    /*
-    public static UUID getUUID(ProxiedPlayer player) {
-        return getUUID(player.getName());
-    }
-    */
-
-    /**
-     * Returns the UUID of the searched player.
-     *
-     * @param player The player.
-     * @return The UUID of the given player.
-     */
-    //Uncomment this if you want the helper method for Bukkit/Spigot:
-    /*
-    public static UUID getUUID(Player player) {
-        return getUUID(player.getName());
-    }
-    */
 
     /**
      * Returns the UUID of the searched player.
