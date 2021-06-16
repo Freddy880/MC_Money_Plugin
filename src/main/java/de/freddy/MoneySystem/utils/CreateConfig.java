5a843b57b5ea2fb65e5af7730fdc588ec16daf44
Freddy880 <81533091+Freddy880@users.noreply.github.com>
2021-06-16 15:34:33 +0200

    Adding new Branch
    -to make a default config

---
+++ src/main/java/de/freddy/MoneySystem/utils/CreateConfig.java
@@ -0,0 +1,11 @@
+package de.freddy.MoneySystem.utils;
+
+public class CreateConfig {
+    public static CreateConfig INSTANCE;
+    public CreateConfig(){
+        INSTANCE = this;
+    }
+    public void makeConfig(){
+
+    }
+}
