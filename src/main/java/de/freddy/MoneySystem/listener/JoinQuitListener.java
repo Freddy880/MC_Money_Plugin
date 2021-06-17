5642d7ee20033ceffffc68cb693c2d416d8ae62e
Freddy880 <81533091+Freddy880@users.noreply.github.com>
2021-06-17 15:41:09 +0200

    Bugfixes

--- src/main/java/de/freddy/MoneySystem/listener/JoinQuitListener.java
+++ src/main/java/de/freddy/MoneySystem/listener/JoinQuitListener.java
@@ -1,22 +1,18 @@
 package de.freddy.MoneySystem.listener;
 
 import de.freddy.MoneySystem.Main;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class JoinQuitListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
-        player.performCommand("spawn");
-        player.getInventory().setHelmet(new ItemStack(Material.ACACIA_BOAT));
-                event.setJoinMessage("§a§l+§4§r " + player.getDisplayName() + " " + Main.INSTANCE.config().getString("Join Message"));
+        event.setJoinMessage("§a§l+§4§r " + player.getDisplayName() + " " + Main.INSTANCE.config().getString("Join Message"));
 
         player.sendMessage("Zurzeit online: ");
 
